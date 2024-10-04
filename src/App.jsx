4da099e22badbd9ce382e5b754ffd8e6b3f2ac49
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-04 17:19:14 +0600

    project setup donr

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto' >
+      <h1 className="text-3xl  underline text-white">Hello World</h1>
+    </main>
+  )
+}
+
+export default App
