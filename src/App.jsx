9f9b96183054f58f9a5a329da93686216bc8225e
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-04 18:48:23 +0600

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto' >
-      <h1 className="text-3xl  underline text-white">Hello World</h1>
+      <Navbar/>
     </main>
   )
 }
