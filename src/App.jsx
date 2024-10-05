ebfc68cc13c5f8c398fc538f3626cc74b4dbe113
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-05 20:59:13 +0600

    contact section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Footer from "./sections/Footer"
-import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
+import Hero from "./sections/Hero"
+import Footer from "./sections/Footer"
+import Contact from "./sections/Contact"
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto' >
       <Navbar/>
       <Hero/>
+      <Contact/>
       <Footer/>
     </main>
   )
