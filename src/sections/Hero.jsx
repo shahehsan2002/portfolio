24ee70e1e9829ced55bb5617b211e49cc2871cf9
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-06 16:03:44 +0600

    footer section updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Ehsan <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+        <p className="hero_tag text-gray_gradient">Developing Products, Defining Brands</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
